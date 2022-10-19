1b96a4374c997803f5de28fe7f374d3ce25cb280
NovikovaNadezda <114093562+NovikovaNadezda@users.noreply.github.com>
2022-10-19 18:33:44 +0300

    Add files via upload

---
+++ Source1.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	//Series1◦
+	/*//.Даны десять вещественных чисел.Найти их сумму.*/
+	/*int sum = 0;
+	int x;
+	for (int i = 1; i <= 10; i++) {
+		cin >> x;
+		sum += x;
+	}cout << sum << endl;*/
+	/*//Series2.Даны десять вещественных чисел.Найти их произведение.*/
+	/*int proisv =1;
+	int x;
+	int i = 1;
+	while (i <= 10) {
+		i++;
+		cin >> x;
+		proisv *= x;
+
+	}cout << proisv << endl;*/
+	/*//Series3.Даны десять вещественных чисел.Найти их среднее арифметическое.*/
+	/*int sum = 0;
+	int x;
+	for (int i = 1; i <= 10; i++) {
+		cin >> x;
+		sum += x;
+		
+	}int sr = sum / 10;
+	cout << sr << endl;*/
+	/*//Series4.Дано целое число N и набор из N вещественных чисел.Вывести
+	//сумму и произведение чисел из данного набора.*/
+	/*int proisv = 1;
+	int n;
+	cin >> n;
+	int i = 1;
+	int x;
+	int sum=0;
+	while (i <= n) {
+		
+		cin >> x;
+		proisv *= x;
+		 sum += x;
+         i++;
+	}
+	cout << sum <<" "<< proisv << endl;*/
+	/*//Series5.Дано целое число N и набор из N положительных вещественных
+	//чисел.Вывести в том же порядке целые части всех чисел из данного
+	//набора(как вещественные числа с нулевой дробной частью), а также
+	//сумму всех целых частей.*/
+	int n;
+	int x;
+	cin >> n;
+	n / 1;
+	for (int i = 1; i <= n; i++) {
+	cout<<}cout <<
+}
